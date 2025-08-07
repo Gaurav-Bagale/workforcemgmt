ef3037cb44ca2b5f4b4f19cdd14d744389dc4243
gaurav <gauravbagale77@gmail.com>
2025-08-07 15:04:43 +0530

    full project setup

---
+++ src/main/java/com/railse/hiring/workforcemgmt/service/TaskManagementService.java
@@ -0,0 +1,22 @@
+package com.railse.hiring.workforcemgmt.service;
+import com.railse.hiring.workforcemgmt.dto.*;
+import com.railse.hiring.workforcemgmt.model.TaskManagement;
+
+import java.util.List;
+
+
+public interface TaskManagementService {
+
+    List<TaskManagementDto> createTasks(TaskCreateRequest request);
+
+    List<TaskManagementDto> updateTasks(UpdateTaskRequest request);
+
+    String assignByReference(AssignByReferenceRequest request);
+
+    List<TaskManagementDto> fetchTasksByDate(TaskFetchByDateRequest request);
+
+    TaskManagementDto findTaskById(Long id);
+
+
+    List<TaskManagementDto> findAll();
+}
